aef7e8583262348bfebc9b062e46029e596dda44
kc1r74p <7422353+kc1r74p@users.noreply.github.com>
2020-03-07 23:02:28 +0100

    Create run_render.sh

---
+++ scripts/run_render.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set +x
+apt-get update 2>&1 >/dev/null
+apt-get install -y pv 2>&1 >/dev/null 
+apt-get install -y cifs-utils 2>&1 >/dev/null
+apt-get install -y --update nodejs npm 2>&1 >/dev/null
+#apt install ffmpeg 2>&1 >/dev/null 
+mkdir /mnt/ingress
+mount -t cifs ((nas_adr)) /mnt/ingress -o vers=3.0,username=((nas_adm)),password=((nas_pw)),dir_mode=0777,file_mode=0777,serverino
+cd auto-renderer
+echo "Installing render deps..."
+npm i 2>&1 >/dev/null 
+echo "Building app..."
+npm run build
+cd dist
+mkdir in
+mkdir out
+mkdir final
+cd in
+echo "Copying ingress into container input..."
+pv /mnt/ingress/ingress/* > ./*
+cd ..
+cd ..
+echo "Running auto render..."
+npm start
+echo      
+echo "-------------------------"
+cd dist
+cd final
+echo "Retriving container output to share..."
+pv ./* > /mnt/ingress/
+echo      
+echo      
+echo "-------------------------"
+echo      
+echo "done!"
+echo      
